3fba7e65169dead32f0b5937b1ac163dd38424c1
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-02-27 12:40:09 +0300

    1 task in process

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+//
+const galleryEl = document.querySelector(".gallery");
+
+function createItems() {
+  const items = [];
+
+  items.forEach((element) => {
+    const { preview, originalSize, description } = elment;
+  });
+}
