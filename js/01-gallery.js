a8243a19394d09f2a0a5d00fca6e67f79189bf72
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-02-27 15:42:19 +0300

    task 01 -in progress

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ import { galleryItems } from "./gallery-items.js";
 const galleryContainer = document.querySelector(".gallery");
 const gallerMarkup = creatGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", gallerMarkup);
-galleryContainer.addEventListener("click", onLinkClick);
+// galleryContainer.addEventListener("click", onLinkClick);
 
 function creatGalleryMarkup(items) {
   return galleryItems
@@ -25,11 +25,16 @@ function creatGalleryMarkup(items) {
 }
 
 console.log(galleryItems);
+
 function onLinkClick(e) {
   e.preventDefault;
-  const instance = basicLightbox.create(`
-      <img src="assets/images/image.png" width="800" height="600">
-  `);
-
-  instance.show();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
 }
+//   const instance = basicLightbox.create(`
+//       <img src="assets/images/image.png" width="800" height="600">
+//   `);
+
+//   instance.show();
+// }
